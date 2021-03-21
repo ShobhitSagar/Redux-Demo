57abdfc91742d16415bcef6025f84f5c5746a1cf
ShobhitSagar <shobhitsagar109@gmail.com>
2021-03-21 10:44:39 +0530

    Proper way of using Redux in React.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { increment, decrement, logged } from './actions'
 
 function App() {
+	const counter = useSelector((state) => state.counter)
+	const isLogged = useSelector((state) => state.isLogged)
+	const dispatch = useDispatch()
+
 	return (
 		<div className="App">
-			<h1>Hello</h1>
+			<h1>Count: {counter}</h1>
+			<button onClick={() => dispatch(increment())}>+</button>
+			<button onClick={() => (counter > 0 ? dispatch(decrement()) : null)}>
+				-
+			</button>
+			<br />
+			{isLogged ? (
+				<button onClick={() => dispatch(logged())}>Logout</button>
+			) : (
+				<button onClick={() => dispatch(logged())}>Login</button>
+			)}
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -3,43 +3,19 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { createStore } from 'redux'
+import allReducer from './reducers'
+import { Provider } from 'react-redux'
 
-// ACTIONS : Name os the services
-const increment = () => {
-	return {
-		type: 'INCREMENT',
-	}
-}
-const decrement = () => {
-	return {
-		type: 'DECREMENT',
-	}
-}
-
-// REDUCER : Logic of actions
-const counter = (state = 0, action) => {
-	switch (action.type) {
-		case 'INCREMENT':
-			return state + 1
-		case 'DECREMENT':
-			return state - 1
-	}
-}
-
-// STORE : Global State
-let store = createStore(counter)
-
-// DISPLAY to console
-store.subscribe(() => console.log(store.getState()))
-
-// DISPATCH : Calling of the actions
-store.dispatch(increment())
-store.dispatch(decrement())
-store.dispatch(decrement())
+let store = createStore(
+	allReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
